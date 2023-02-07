084b6d6fc697c0a6751e4b0cbf047d075253c2fe
Vinícius Passacantili <108286143+iguanas2@users.noreply.github.com>
2023-02-07 17:27:18 -0300

    Adicionando o Robotron 2000 + Desafios
    Adicionando o Site Robotron 2000 junto com os exercícios que eu fiz durante essa formação :)

---
+++ DESAFIO 2/RESPOSTA.txt
@@ -0,0 +1,4 @@
+const calcular = document.getElementById('calcular');
+const resultado = document.querySelector('.resultado');
+
+document.addEventListener("click", (evento) => {resultado.innerHTML = "Fui Clicado"})
---
+++ DESAFIO 4/script.js
@@ -0,0 +1,10 @@
+console.log('fui executado');
+
+const lista = document.querySelector("ul");
+document.querySelector("#botao").addEventListener("click", () => {
+  lista.setAttribute("data-lista", "mostrar");
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  lista.setAttribute("data-lista", "esconder");
+});
